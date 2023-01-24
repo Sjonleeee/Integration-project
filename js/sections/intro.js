5e9b533c2ae39726b25e9fadd1cf35912367a334
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-24 23:09:30 +0100

    Add styline from section features mobile

--- js/sections/intro.js
+++ js/sections/intro.js
@@ -1,7 +1,12 @@
-
 const intro = () => {
-  gsap.from([".intro__img"], {
+  gsap.from(".intro__img", {
     rotate: 2,
+    duration: 1,
+    yoyo: true,
+    repeat: -1,
+  });
+  gsap.to(".arrows", {
+    y: 10,
     duration: 1,
     yoyo: true,
     repeat: -1,
