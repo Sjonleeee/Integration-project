badfb006653b6b622aee1fae7fd055dc170d00ea
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-26 16:18:31 +0100

    Adding animation for section features

--- js/sections/features.js
+++ js/sections/features.js
@@ -1,5 +1,34 @@
-const features = () => {
-  console.log("Features test");
+const features = (gsap) => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".background__features",
+      scrub: true,
+      markers: true,
+      start: "top 70%",
+      end: "top 0%",
+    },
+  });
+
+  tl.from([".features__love__the"], {
+    x: 400,
+    duration: 10,
+  });
+  tl.from([".features__love__features"], {
+    x: -400,
+    duration: 10,
+  });
+
+   tl.from([".features__love__adolphe"], {
+     x: 400,
+     duration: 10,
+   });
+
+  tl.from([".loves"], {
+    x: -400,
+    duration: 10,
+  });
+
+
 };
 
 export default features;
