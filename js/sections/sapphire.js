f82f4eee1408dafed22f129e31163bc0e0f4680b
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-27 23:28:20 +0100

    Add click function show and close

--- js/sections/sapphire.js
+++ js/sections/sapphire.js
@@ -1,9 +1,18 @@
 const sapphire = (gsap) => {
-  document
-    .querySelector("#mouth__piece--click")
-    .addEventListener("click", () => {
-      console.log("sdfsd");
-    });
+  //   document
+  //     .querySelector("#mouth__piece--click")
+  //     .addEventListener("click", () => {});
+
+  const mouthBtn = document.querySelector("#mouth__piece--click");
+  const closeBtn = document.querySelector(".closeIcon");
+  const mouthInfo = document.querySelector(".sapphire__mouth--info");
+  mouthBtn.addEventListener("click", () => {
+    mouthInfo.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mouthInfo.style.display = "none";
+  });
 };
 
 export default sapphire;
