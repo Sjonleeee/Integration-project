a1e64ddb671a3b6677876cb3369afd4f9ef15115
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-28 12:31:22 +0100

    Add javascript for sapphire__body onclick to get information and close onclick

--- js/sections/sapphire.js
+++ js/sections/sapphire.js
@@ -4,14 +4,29 @@ const sapphire = (gsap) => {
   //     .addEventListener("click", () => {});
 
   const mouthBtn = document.querySelector("#mouth__piece--click");
-  const closeBtn = document.querySelector(".closeIcon");
+  const mouthBtnClose = document.querySelector(".mouthCloseIcon");
   const mouthInfo = document.querySelector(".sapphire__mouth--info");
+
+  const bodyBtn = document.querySelector("#body--click");
+  const bodyBtnClose = document.querySelector(".bodyCloseIcon");
+  const bodyInfo = document.querySelector(".sapphire__body--info");
+
+  // Mouth
   mouthBtn.addEventListener("click", () => {
     mouthInfo.style.display = "flex";
   });
 
-  closeBtn.addEventListener("click", () => {
+  mouthBtnClose.addEventListener("click", () => {
     mouthInfo.style.display = "none";
+  });
+
+  //  Body
+  bodyBtn.addEventListener("click", () => {
+    bodyInfo.style.display = "flex";
+  });
+
+  bodyBtnClose.addEventListener("click", () => {
+    bodyInfo.style.display = "none";
   });
 };
 
