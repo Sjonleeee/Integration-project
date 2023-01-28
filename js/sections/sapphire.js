19108509d0b913939d590e3364010edcc8e7574c
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-28 13:44:43 +0100

    Add bell content and styling

--- js/sections/sapphire.js
+++ js/sections/sapphire.js
@@ -1,45 +1,56 @@
 const sapphire = (gsap) => {
-  //   document
-  //     .querySelector("#mouth__piece--click")
-  //     .addEventListener("click", () => {});
-
+  // Mouth buttons
   const mouthBtn = document.querySelector("#mouth__piece--click");
   const mouthBtnClose = document.querySelector(".mouthCloseIcon");
   const mouthInfo = document.querySelector(".sapphire__mouth--info");
 
+  // Lefthand buttons
   const leftHandBtn = document.querySelector("#leftHand--click");
   const leftHandBtnClose = document.querySelector(".leftHandCloseIcon");
   const leftHandInfo = document.querySelector(".sapphire__leftHand--info");
 
+  // Body buttons
   const bodyBtn = document.querySelector("#body--click");
   const bodyBtnClose = document.querySelector(".bodyCloseIcon");
   const bodyInfo = document.querySelector(".sapphire__body--info");
 
-  // Mouth
+  // Right hand buttons
+  const rightHandBtn = document.querySelector("#rightHand--click");
+  const rightHandBtnClose = document.querySelector(".rightHandCloseIcon");
+  const rightHandInfo = document.querySelector(".sapphire__rightHand--info");
+
+  // ------------------------------------------------------------------
+  // Mouth click
   mouthBtn.addEventListener("click", () => {
     mouthInfo.style.display = "flex";
   });
-
   mouthBtnClose.addEventListener("click", () => {
     mouthInfo.style.display = "none";
   });
 
-  // LeftHand
+  // LeftHand click
   leftHandBtn.addEventListener("click", () => {
     leftHandInfo.style.display = "flex";
   });
-
   leftHandBtnClose.addEventListener("click", () => {
     leftHandInfo.style.display = "none";
   });
 
-  //  Body
+  //  Body click
   bodyBtn.addEventListener("click", () => {
     bodyInfo.style.display = "flex";
   });
-
   bodyBtnClose.addEventListener("click", () => {
     bodyInfo.style.display = "none";
+  });
+
+  //  Righthand click
+  rightHandBtn.addEventListener("click", () => {
+    rightHandInfo.style.display = "flex";
+  });
+
+  rightHandBtnClose.addEventListener("click", () => {
+    rightHandInfo.style.display = "none";
   });
 };
 
