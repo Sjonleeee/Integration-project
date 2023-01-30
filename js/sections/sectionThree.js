cae11f73685d00ababfe0ac525f784223aeb5a3d
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-30 12:17:46 +0100

    Add animation section SectionThree

--- js/sections/sectionThree.js
+++ js/sections/sectionThree.js
@@ -3,55 +3,57 @@ const sectionThree = (gsap) => {
   mm.add(
     {
       isMobile: "(max-width: 390px)",
-      isIpad: "(min-width: 834px)",
       isDesktop: "(min-width: 1512px)",
     },
     (context) => {
       const { isMobile, isIpad, isDesktop } = context.conditions;
-      // Hier moet content
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".cutted__background",
+          scrub: true,
+          // markers: true,
+          start: "top 30%",
+          end: "bottom -15%",
+        },
+      });
+      gsap.from(".flowers", {
+        scale: 1.1,
+        duration: 0.6,
+        yoyo: true,
+        repeat: -1,
+      });
+
+      tl.from(".sectionThree__adolpheSax", {
+        x: isMobile ? -400 : -800,
+        x: isDesktop ? -1150 : -1500,
+        duration: 10,
+      });
+
+      tl.from(".flowers", {
+        opacity: 0,
+        duration: 10,
+      });
+
+      tl.to(".head__adolphe", {
+        x: isMobile ? 300 : 400,
+        x: isDesktop ? 850 : 1000,
+
+        duration: 5,
+      });
+
+      tl.to(".saxophone", {
+        x: isMobile ? -300 : -400,
+        x: isDesktop ? -750 : -800,
+
+        duration: 5,
+      });
+
+      tl.from(".sectionThree__sapphire", {
+        x: isMobile ? 400 : 800,
+        duration: 10,
+      });
     }
   );
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cutted__background",
-      scrub: true,
-      // markers: true,
-      start: "top 30%",
-      end: "bottom -15%",
-    },
-  });
-
-  gsap.from(".flowers", {
-    scale: 1.1,
-    duration: 0.6,
-    yoyo: true,
-    repeat: -1,
-  });
-
-  tl.from(".sectionThree__adolpheSax", {
-    x: -400,
-    duration: 10,
-  });
-
-  tl.from(".flowers", {
-    opacity: 0,
-    duration: 10,
-  });
-
-  tl.to(".head__adolphe", {
-    x: 300,
-    duration: 5,
-  });
-
-  tl.to(".saxophone", {
-    x: -300,
-    duration: 5,
-  });
-
-  tl.from(".sectionThree__sapphire", {
-    x: 400,
-    duration: 10,
-  });
 };
 
 export default sectionThree;
