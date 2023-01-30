699b9b2d00879234c2397ea17f0b13d7ddfd02d3
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-30 12:17:21 +0100

    Add animation section goToConcerts

--- js/sections/goToConcert.js
+++ js/sections/goToConcert.js
@@ -1,4 +1,46 @@
-const goToConcert = () => {
+const goToConcert = (gsap) => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".goToConcerts__adolpheAndSapphire",
+      scrub: true,
+    //   markers: true,
+      start: "top 30%",
+      end: "top 0%",
+    },
+  });
+  tl.from(".goToConcerts__adolpheAndSapphire", {
+    y: -200,
+    opacity: 0,
+    duration: 5,
+  });
+  tl.from(".goToConcerts__go", {
+    x: -200,
+    opacity: 0,
+    duration: 5,
+  });
+  tl.from(".goToConcerts__inTheir", {
+    x: 200,
+    opacity: 0,
+    duration: 5,
+  });
+
+  tl.from(".goToConcerts__title__freeTime", {
+    x: -200,
+    opacity: 0,
+    duration: 5,
+  });
+  tl.from(".goToConcerts__information", {
+    y: -50,
+    opacity: 0,
+    duration: 5,
+  });
+  gsap.from(".goToConcerts__arrow", {
+    scale:1.2,
+    y: 10,
+    duration: 0.6,
+    yoyo: true,
+    repeat: -1,
+  });
 };
 
 export default goToConcert;
