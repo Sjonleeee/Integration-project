a00a30e6925ff4b600a7259e8fc87dca524585ed
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-30 12:17:03 +0100

    Add animation section getInTouch

--- js/sections/getInTouch.js
+++ js/sections/getInTouch.js
@@ -1,4 +1,30 @@
-const getInTouch = () => {
+const getInTouch = (gsap) => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".goToConcerts__join",
+      scrub: true,
+      // markers: true,
+      start: "top 50%",
+      end: "top 0%",
+    },
+  });
+
+  tl.from(".ticket--1", {
+    y: 20,
+    opacity: 0,
+    duration: 2,
+  });
+
+  tl.from(".ticket--2", {
+    y: 20,
+    opacity: 0,
+    duration: 3,
+  });
+  tl.from(".ticket--3", {
+    y: 20,
+    opacity: 0,
+    duration: 4,
+  });
 };
 
 export default getInTouch;
