c24bb460b58cc7fc622841394981c5e69f45a2be
MinhTriHa <minh.tri.ha@student.howest.be>
2023-01-30 02:01:43 +0100

    Adding animation for sapphire.js

--- js/sections/family.js
+++ js/sections/family.js
@@ -1,4 +1,59 @@
-const family = () => {
-};
+const family = (gsap) => {
+  const mm = gsap.matchMedia();
+  mm.add(
+    {
+      isMobile: "(max-width: 390px)",
+      isIpad: "(min-width: 834px)",
+      isDesktop: "(min-width: 1512px)",
+    },
+    (context) => {
+      const { isMobile, isIpad, isDesktop } = context.conditions;
+      const tl = gsap.timeline({
+        repeatDelay: 1,
+        scrollTrigger: {
+          trigger: ".family__p",
+          scrub: true,
+          markers: true,
+          start: "top 20%",
+          end: "bottom -20%",
+        },
+      });
 
+      tl.from(".family__p--1", {
+        opacity: 0,
+        y: -300,
+        duration: 8,
+      });
+
+      tl.from(".family__title__soThey", {
+        opacity: 0,
+        y: -100,
+        duration: 8,
+      });
+
+      tl.from(".family__title__decide", {
+        opacity: 0,
+        y: -100,
+        duration: 8,
+      });
+      tl.from(".family__title__family", {
+        opacity: 0,
+        y: -100,
+        duration: 8,
+      });
+      gsap.from(".family__p--2", {
+        scrollTrigger: {
+          trigger: ".family__title__decide",
+          scrub: true,
+          markers: true,
+          start: "top 20%",
+          end: "bottom -20%",
+        },
+        opacity: 0,
+        y: -100,
+        delay: 5,
+      });
+    }
+  );
+};
 export default family;
